7f4a5b07ac72ad24e1b95a64102caa6533605628
bob1983 <bob.1983.g@gmail.com>
2020-04-09 22:34:49 +0900

    Added programsService

---
+++ src/services/programs.service.spec.ts
@@ -0,0 +1,38 @@
+import { ProgramsService } from './programs.service'
+import { OnsenClient } from '../lib/onsen_client'
+
+describe('Programs', () => {
+  describe('fetchPrograms', () => {
+    it('returns programs', async () => {
+      jest.spyOn(OnsenClient.prototype, 'fetchPrograms').mockResolvedValueOnce([
+        'mhr3',
+        'stb'
+      ])
+      const service = new ProgramsService()
+      const programs = await service.fetchPrograms()
+      expect(programs).toEqual(['mhr3', 'stb'])
+    })
+
+    it('does not include niconama', async () => {
+      jest.spyOn(OnsenClient.prototype, 'fetchPrograms').mockResolvedValueOnce([
+        'stb',
+        'niconama_stb',
+        'stb_niconama'
+      ])
+      const service = new ProgramsService()
+      const programs = await service.fetchPrograms()
+      expect(programs).toEqual(['stb']);
+    })
+
+    it('does not include program end with _ex', async () => {
+      jest.spyOn(OnsenClient.prototype, 'fetchPrograms').mockResolvedValueOnce([
+        'gashitai',
+        '_ex_sample',
+        'gashitai_ex'
+      ])
+      const service = new ProgramsService()
+      const programs = await service.fetchPrograms()
+      expect(programs).toEqual(['gashitai', '_ex_sample']);
+    })
+  })
+})
---
+++ src/services/programs.service.ts
@@ -0,0 +1,20 @@
+import { OnsenClient } from '../lib/onsen_client'
+
+export class ProgramsService {
+  client: OnsenClient
+
+  constructor() {
+    this.client = OnsenClient.client()
+  }
+
+  async fetchPrograms(): Promise<string[]> {
+    const programs = await this.client.fetchPrograms()
+    return programs
+      .filter(name => {
+        return !name.match(/niconama/g)
+      })
+      .filter(name => {
+        return !name.match(/_ex$/)
+      })
+  }
+}
